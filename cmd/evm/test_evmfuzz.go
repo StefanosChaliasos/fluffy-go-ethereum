1299450fa4ee6db21b643bff60a48857cc4189cd
Stefanos Chaliasos <stefanoshaliassos@gmail.com>
2022-09-30 17:51:00 +0300

    Add fuzzing functionality and test program

---
+++ cmd/evm/test_evmfuzz.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/ethereum/go-ethereum/tests"
+	"google.golang.org/protobuf/encoding/prototext"
+	"google.golang.org/protobuf/proto"
+	"io/ioutil"
+	_ "os"
+	_ "testing"
+	_ "time"
+)
+
+var inputPipe string
+var outputPipe string
+
+func init() {
+	flag.StringVar(&inputPipe, "in", "", "")
+	flag.StringVar(&outputPipe, "out", "", "")
+}
+
+func main() {
+	dirname := "../openethereum/crates/evmfuzz/fuzz/corpus/fuzz_target_1"
+	files, err := ioutil.ReadDir(dirname)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, file := range files {
+		println("PROCESSING: " + dirname + "/" + file.Name())
+		fuzzedInput, err := ioutil.ReadFile(dirname + "/" + file.Name())
+		if err != nil {
+			panic(err)
+		}
+
+		fuzzedInputProto := tests.Fuzzed{}
+		err = proto.Unmarshal(fuzzedInput, &fuzzedInputProto)
+		if err != nil {
+			panic(err)
+		}
+
+		fuzzResults := tests.RunFuzz(fuzzedInputProto)
+		marshalOptions := prototext.MarshalOptions{}
+		fuzzResultText := marshalOptions.Format(&fuzzResults)
+		fmt.Println(fuzzResultText)
+	}
+}
